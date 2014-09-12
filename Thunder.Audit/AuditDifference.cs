ccd92e3bb52f7e56eee0ee0d260efa7bdbc658f3
Adriano Caldeira <adriano@tribecca.com.br>
2014-09-12 16:37:11 -0300

    leitura de componenttype

--- Thunder.Audit/AuditDifference.cs
+++ Thunder.Audit/AuditDifference.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using NHibernate.Event;
+using NHibernate.Type;
 
 namespace Thunder.Audit
 {
@@ -16,17 +18,50 @@ namespace Thunder.Audit
                 return new List<AuditDifference>();
 
             var indexes = @event.Persister.FindDirty(@event.State, @event.OldState, @event.Entity, @event.Session);
+            var list = new List<AuditDifference>();
 
-            return (from index in indexes
-                let oldValue = GetValue(@event.OldState, index)
-                let newValue = GetValue(@event.State, index)
-                where oldValue != newValue
-                select new AuditDifference
+            foreach (var index in indexes)
+            {
+                if (@event.Persister.PropertyTypes[index] is ComponentType)
                 {
-                    NewValue = newValue, 
-                    OldValue = oldValue, 
-                    Property = @event.Persister.PropertyNames[index]
-                }).ToList();
+                    var oldObject = @event.OldState[index];
+                    var newObject = @event.State[index];
+
+                    foreach (var property in newObject.GetType().GetProperties())
+                    {
+                        if (!property.CanRead) continue;
+
+                        var newValue = property.GetValue(newObject, null) == null ? "" : property.GetValue(newObject, null).ToString();
+                        var oldValue = oldObject == null || property.GetValue(oldObject, null) == null ? "" : property.GetValue(oldObject, null).ToString();
+
+                        if (oldValue == newValue) continue;
+
+                        list.Add(new AuditDifference
+                        {
+                            NewValue = newValue,
+                            OldValue = oldValue,
+                            Property = @event.Persister.PropertyNames[index] + "." + property.Name
+                        });
+                    }
+                }
+                else
+                {
+                    var oldValue = GetValue(@event.OldState, index);
+                    var newValue = GetValue(@event.State, index);
+
+                    if (oldValue != newValue)
+                    {
+                        list.Add(new AuditDifference
+                        {
+                            NewValue = newValue,
+                            OldValue = oldValue,
+                            Property = @event.Persister.PropertyNames[index]
+                        });
+                    }
+                }
+
+            }
+            return list;
         }
 
         private static string GetValue(IList<object> stateArray, int position)
